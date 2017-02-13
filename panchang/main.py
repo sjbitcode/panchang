a79dc87e695002f9cd777195d2eb7c1b85413330
gitsomecode <sjbitcode@gmail.com>
2017-02-12 23:11:15 -0500

    Clean up

--- panchang/main.py
+++ panchang/main.py
@@ -1,6 +1,5 @@
 import logging
 import logging.config
-
 from celery import Celery
 
 from .settings import LOG_SETTINGS, MODULE_NAME
@@ -8,12 +7,9 @@ from .settings import LOG_SETTINGS, MODULE_NAME
 
 # Configure celery
 celery = Celery(MODULE_NAME)
-celery.config_from_object('{}.{}'.format(MODULE_NAME, 'settings'))
+celery.config_from_object('{}.settings'.format(MODULE_NAME))
 celery.autodiscover_tasks([MODULE_NAME])
+
 
 # Configure logger
 logging.config.dictConfig(LOG_SETTINGS)
-
-
-# Run celery worker server
-# celery -A panchang.main worker --loglevel=info --beat
